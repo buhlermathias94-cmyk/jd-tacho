2c2e109f51593f7e5b46a9c941cc1696ec2607c6
buhlermathias94-cmyk <buhlermathias94@gmail.com>
2026-05-14 15:16:00 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,14 +1,13 @@
-const CACHE = 'jd-tacho-v11';
-const ASSETS = ['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
-self.addEventListener('install', event => {
-  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));
-  self.skipWaiting();
-});
-self.addEventListener('activate', event => {
-  event.waitUntil(self.clients.claim());
-});
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(resp => resp || fetch(event.request))
+const CACHE = 'jd-tacho-v10';
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(cache => {
+      return cache.addAll([
+        './',
+        './index.html',
+        './manifest.webmanifest'
+      ]);
+    })
   );
 });
