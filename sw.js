fac323d5c1d70df98c7f624dd91d0b244fc489de
buhlermathias94-cmyk <buhlermathias94@gmail.com>
2026-05-14 15:01:21 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'jd-tacho-v7';
+const CACHE = 'jd-tacho-v8';
 const ASSETS = ['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));
